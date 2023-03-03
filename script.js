a54eb93223f4f61a57697e228cc43f208cf92ff3
annyn <ennibondarenko@gmail.com>
2023-03-03 18:14:44 +0400

    done

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const error = document.getElementById("error");
-const submit = document.getElementById("submit");
+const submit = document.getElementById("submitArrow");
 
 
 
-function validateFunction(){
 
-    
-document.getElementById('email').addEventListener('click', validateFunction);
 
+document.getElementById("submitArrow").addEventListener('click', validation);
+document.getElementById("email").addEventListener('click', isEmpty);
+
+function validation() {
     let regx = /^([a-z 0-9\.-]+)@([a-z0-9]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
-
-    if(regx.test(email.value)){
-
-
-    }
-    
-
-    document.getElementById("email").addEventListener("click", isEmpty);
-    function isEmpty(){
-        if(email.value == ""){
-            document.getElementById("error").style.visibility = "visible";
-            document.getElementById("error").style.color="red";
-        }
+   
+    if (regx.test(email.value)) {
+        document.getElementById("error").style.visibility = "hidden";
+        document.getElementById("email").style.border = "1px solid #CE9898";
+        document.getElementById("email").style.backgroundImage = "none";
+    } else{
+        document.getElementById("error").style.visibility = "visible";
+        document.getElementById("error").innerText = "Please provide a valid email"
+        document.getElementById("error").style.color = "#F96464";
+        document.getElementById("email").style.border = "1px solid #F96464";
+        document.getElementById("email").style.backgroundImage = "url(/images/icon-error.svg)";
     }
 }
 
+function isEmpty() {
+    if (email.value == "") {
+        document.getElementById("email").style.backgroundImage = "none";
+        document.getElementById("email").style.border = "1px solid #CE9898";
+        document.getElementById("error").style.visibility = "hidden";
+    }
+}
